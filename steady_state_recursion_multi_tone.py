be4a82d3cb4c23a7e354fe9398428ed6d86b60a1
Brian Baker <BrianBaker2021@u.northwestern.edu>
2018-07-23 10:24:16 -0500

    added a multitone module

--- steady_state_recursion_multi_tone.py
+++ steady_state_recursion_multi_tone.py
@@ -24,25 +24,26 @@ class DrivenOpenSystem:
 
     def __init__(self, hamiltonian, decoherence_rates, drive, frequency):
 
-        if isinstance(hamiltonian, Qobj) and isinstance(drive, Qobj):
+        if isinstance(hamiltonian, Qobj):
             self.hamiltonian = hamiltonian
-            self.v = drive
         else:
-            raise TypeError("The system hamiltonian and drive must both be a Qobj. Try again.")
+            raise TypeError("The system hamiltonian must be a Qobj. Try again.")
             
         self.dim = self.hamiltonian.shape[0]
+        self.number_of_modes = len(frequency)
         self.gamma_table = decoherence_rates
-        self.omega = frequency
+        self.omega = frequency # this is a list of frequencies
         eigensystem = self.hamiltonian.eigenstates()
         self.evecs = eigensystem[1]
         self.evals = eigensystem[0]
+        self.v = drive # this is a list of drives for each frequency
 
         self.jump_ops = self.jump_operators(self.gamma_table)
         self.rho0 = steadystate(self.hamiltonian, self.jump_ops)
         self.density_matrix = [[(self.evecs[a].dag() * self.rho0 * self.evecs[b])[0] for b in range(self.dim)] for a in range(self.dim)]
         self.density_matrix = np.asarray(self.density_matrix)
 
-        self.integer_list = [0 for k in range(self.dim)]
+        self.integer_list = [[0 for k in range(self.dim)] for j in range(self.number_of_modes)] # matrix of integers
         self.drive_term_list = []
         self.rotating_frame_hamiltonian = self.hamiltonian
 
@@ -102,23 +103,25 @@ class DrivenOpenSystem:
     This quantity appears when solving for the first order perturbative correction to 
     the density matrix.
     """
-    def calculate_first_order_correction(self, v_min, n, m, L0):
+    def calculate_first_order_correction(self, v_min, n, m, j, L0):
         if n >= m:
             return 0.0
         evecs = self.evecs
         evals = self.evals
 
-        v_nm = (evecs[n].dag()*(self.v*evecs[m]))[0][0][0]
+        v_nm = (evecs[n].dag()*(self.v[j]*evecs[m]))[0][0][0]
         if abs(v_nm)  <= v_min:
             return 0.0
         
         k = self.integer_list
         rho_0 = np.reshape(self.density_matrix,(self.dim**2,1),order='F')
 
-        V_nm = (evecs[n]*evecs[m].dag()*(evecs[n].dag()*(self.v*evecs[m])))
+        V_nm = (evecs[n]*evecs[m].dag()*(evecs[n].dag()*(self.v[j]*evecs[m])))
         L_nm = liouvillian(V_nm)
         b = np.dot(L_nm.full(),rho_0)
-        omega_of_k = (k[n] - k[m] + 1)*self.omega
+        omega_of_k = self.omega[j]
+        for l in range(self.number_of_modes):
+            omega_of_k += self.omega[l] * (k[l][n] - k[l][m])
         A = 1j*omega_of_k * identity(self.dim**2) - L0
         A = A.full()
         rho_correction = la.lstsq(A,b,rcond = 1e-6)[0]
@@ -127,96 +130,116 @@ class DrivenOpenSystem:
     def term_order(self, v_min):
         
         L0 = (liouvillian(self.rotating_frame_hamiltonian, self.jump_ops)).full()
-        R = [[self.calculate_first_order_correction(v_min, n, m, L0) for m in range(self.dim)] for n in range(self.dim)]
-       
+        R = [[[self.calculate_first_order_correction(v_min, n, m, j, L0) for m in range(self.dim)] for n in range(self.dim)] for j in range(self.number_of_modes)]
+      
         R = np.asarray(R)
         # now order terms according to how large their value in R[][] is
-        number_of_transitions = int(self.dim*(self.dim-1)/2)
+        number_of_transitions = int(self.dim*(self.dim-1)/2) * self.number_of_modes
         transition_order = [[] for i in range(number_of_transitions)]
         rank = 0
         for i in range(number_of_transitions):
             R_max = np.where(R == R.max())
-            indices = [R_max[0][0], R_max[1][0]]
+            indices = [R_max[0][0], R_max[1][0], R_max[2][0]]
             transition_order[rank] = [R.max(), indices]
-            R[indices[0]][indices[1]] = R[indices[1]][indices[0]] = 0
+            R[indices[0]][indices[1]][indices[2]] = R[indices[0]][indices[2]][indices[1]] = 0
             rank += 1
         
         # This algorithm assigns an integer to each eigenstate of the hamiltonian based on the ordering from above
         # Initialize Lists:
-        states = [[] for i in range(self.dim)]
+        states = [[[] for i in range(self.dim)] for j in range(self.number_of_modes)]
         # START ALGORITHM
             # initialize first term into a graph
-        first_index = transition_order[0][1]
-        graph_list = [[first_index[0],first_index[1]]]
-        states[max(first_index)] = 1
-        states[min(first_index)] = 0
+        first_indices = transition_order[0][1]
+        first_mode_index = first_indices[0]
+        first_index = first_indices[1:] 
+        graph_list = [[[first_index[0],first_index[1]]] for j in range(self.number_of_modes)]
+        states[first_mode_index][max(first_index)] = 1
+        states[first_mode_index][min(first_index)] = 0
+        for j in range(self.number_of_modes):
+            if j != first_mode_index:
+                states[j][max(first_index)] = 0
+                states[j][min(first_index)] = 0 
             # assign subsequent terms
         for i in range(1,number_of_transitions):
             #print(graph_list)
             if transition_order[i]==[] or transition_order[i][1] ==[0,0]:
                 break
             else:
-                index = transition_order[i][1]
-                if states[index[0]]==states[index[1]]==[]: 
-                    states[max(index)] = 1
-                    states[min(index)] = 0
-                    # creates a new graph
-                    graph_list.append([index[0],index[1]])
-                elif states[index[0]]==[]:
-                    if index[0] > index[1]:
-                        states[index[0]] = states[index[1]] + 1
-                    else:
-                        states[index[0]] = states[index[1]] - 1
-                    for k,graph in enumerate(graph_list):
-                        if index[1] in graph:
-                            graph_list[k].append(index[0]) # place in same graph
-                            break
-                elif states[index[1]]==[]:
-                    if index[0] > index[1]:
-                        states[index[1]] = states[index[0]] - 1
-                    else:
-                        states[index[1]] = states[index[0]] + 1
-                    for k,graph in enumerate(graph_list):
-                        if index[0] in graph:
-                            graph_list[k].append(index[1]) # place in same graph
-                            break
-                else:
-                    for k,graph in enumerate(graph_list):
-                        overlap = list(set(index) & set(graph))
-                        if (len(overlap) == 2):
-                            # loop closure: can't do anything
-                            break
-                        elif (len(overlap) == 1):
-                            fixed_index = overlap[0]
-                            shift_index = list(set(index) - set(graph))[0]
-                            old_integer = states[shift_index]
-                            if shift_index > fixed_index:
-                                new_integer = states[fixed_index] + 1
-                            else:
-                                new_integer = states[fixed_index] - 1
-                            shift_amount = new_integer - old_integer
-                            # shift the whole graph
-                            for j,graph2 in enumerate(graph_list):
-                                if shift_index in graph2:
-                                    for m,index2 in enumerate(graph2):
-                                        states[index2] = states[index2] + shift_amount
-                                    graph_list[k] = graph_list[k] + graph2
-                                    graph_list.pop(j)
-                                    break
-                            break
-                        else:
-                            continue
-                    continue
+                indices = transition_order[i][1]
+                mode_index = indices[0]
+                index = indices[1:]
+                
+                #### 
+                states, graph_list = self.graph_build(states, mode_index, index, graph_list, 1)       
+                for j in range(self.number_of_modes):
+                    if j != mode_index:
+                        states, graph_list = self.graph_build(states, j, index, graph_list, 0)   
         # Buffer for extremely small values of R
-        if [] in states:
-            for i,integer in enumerate(states):
-                if integer == []:
-                    states[i] = 0
+        for j in range(self.number_of_modes):
+            if [] in states[j]:
+                for i,integer in enumerate(states[j]):
+                    if integer == []:
+                        states[j][i] = 0
         #print(transition_order)
         #print(graph_list)
         #print(states)
          ## END algorithm
         return transition_order, states
+
+    def graph_build(self,states,mode_index,index,graph_list,num):
+        if states[mode_index][index[0]]==states[mode_index][index[1]]==[]:
+            states[mode_index][max(index)] = num
+            states[mode_index][min(index)] = 0
+            # creates a new graph
+            graph_list[mode_index].append([index[0],index[1]])
+        elif states[mode_index][index[0]]==[]:
+            if index[0] > index[1]:
+                states[mode_index][index[0]] = states[mode_index][index[1]] + num
+            else:
+                states[mode_index][index[0]] = states[mode_index][index[1]] - num
+            for k,graph in enumerate(graph_list[mode_index]):
+                if index[1] in graph:
+                    graph_list[mode_index][k].append(index[0]) # place in same graph
+                    break
+        elif states[mode_index][index[1]]==[]:
+            if index[0] > index[1]:
+                states[mode_index][index[1]] = states[mode_index][index[0]] - num
+            else:
+                states[mode_index][index[1]] = states[mode_index][index[0]] + num
+            for k,graph in enumerate(graph_list[mode_index]):
+                if index[0] in graph:
+                    graph_list[mode_index][k].append(index[1]) # place in same graph
+                    break
+        else:
+            for k,graph in enumerate(graph_list[mode_index]):
+                overlap = list(set(index) & set(graph))
+                if (len(overlap) == 2):
+                    # loop closure: can't do anything
+                    break
+                elif (len(overlap) == 1):
+                    fixed_index = overlap[0]
+                    shift_index = list(set(index) - set(graph))[0]
+                    old_integer = states[mode_index][shift_index]
+                    if shift_index > fixed_index:
+                        new_integer = states[mode_index][fixed_index] + num
+                    else:
+                        new_integer = states[mode_index][fixed_index] - num
+                    shift_amount = new_integer - old_integer
+                    # shift the whole graph
+                    for j,graph2 in enumerate(graph_list[mode_index]):
+                        if shift_index in graph2:
+                            for m,index2 in enumerate(graph2):
+                                states[mode_index][index2] = states[mode_index][index2] + shift_amount
+                            graph_list[mode_index][k] = graph_list[mode_index][k] + graph2
+                            graph_list[mode_index].pop(j)
+                            break
+                    break
+                else:
+                    continue
+            continue
+
+        return states, graph_list
+
 
     def frame_rotate(self, v_min):
         # Set up 0th order eigenstates and eigenenergies
@@ -227,23 +250,43 @@ class DrivenOpenSystem:
         ordering, states = self.term_order(v_min)
         
         h_undriven = 0
-        for j in range(self.dim):
-            h_undriven += (evals[j])*(evecs[j]*evecs[j].dag()) - self.omega * states[j]*(evecs[j]*evecs[j].dag())
+        
+        for n in range(self.dim):
+            Omega = 0
+            for j in range(self.number_of_modes):
+                Omega += self.omega[j] * states[j][n]
+            h_undriven += (evals[n])*(evecs[n]*evecs[n].dag()) - Omega*(evecs[n]*evecs[n].dag())
 
         # This while loop sets up the drive term in the rotating frame. 
         h_drive = 0
         error = 0
-        number_of_transitions = int(self.dim*(self.dim-1)/2)
+        number_of_transitions = int(self.dim*(self.dim-1)/2) * self.number_of_modes
         drive_terms = []
         neglected_terms = []
         for j in range(number_of_transitions):
-            index = ordering[j][1]
+            indices = ordering[j][1]
+            mode_index = indices[0]
+            index = indices[1:] 
             if (index[0] != index[1]):
-                if (states[max(index)] == states[min(index)] + 1):
-                    h_drive += (evecs[min(index)]*evecs[max(index)].dag()*(evecs[min(index)].dag()*(v*evecs[max(index)])))
-                    drive_terms.append(evecs[min(index)]*evecs[max(index)].dag()*(evecs[min(index)].dag()*(v*evecs[max(index)])))
+                add_term = True
+                for k in range(self.number_of_modes):
+                    if k == mode_index:
+                        if (states[k][max(index)] == states[k][min(index)] + 1):
+                            continue
+                        else:
+                            add_term = False
+                            break
+                    else:
+                        if (states[k][max(index)] == states[k][min(index)]):
+                            continue
+                        else:
+                            add_term = False
+                            break 
+                if add_term:
+                    h_drive += (evecs[min(index)]*evecs[max(index)].dag()*(evecs[min(index)].dag()*(v[mode_index]*evecs[max(index)])))
+                    drive_terms.append(evecs[min(index)]*evecs[max(index)].dag()*(evecs[min(index)].dag()*(v[mode_index]*evecs[max(index)])))
                 else:
-                    neglected_terms.append(evecs[min(index)]*evecs[max(index)].dag()*(evecs[min(index)].dag()*(v*evecs[max(index)])))
+                    neglected_terms.append(evecs[min(index)]*evecs[max(index)].dag()*(evecs[min(index)].dag()*(v[mode_index]*evecs[max(index)])))
                     error += ordering[j][0]**2
             else:
                 break
